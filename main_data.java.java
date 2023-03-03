0c1f587548df2dbaaaa9bbda44ae345b83975245
PantelisRoum <104692010+PantelisRoum@users.noreply.github.com>
2023-03-03 22:07:17 +0200

    Add files via upload

---
+++ main_data.java.java
@@ -0,0 +1,8 @@
+//this is the main programm 
+public class Main{
+    public static void main(String[] args){
+        Date date_1=new Date(7,2,2023);
+        date_1.display(7,2,2023);
+        
+    }
+}
