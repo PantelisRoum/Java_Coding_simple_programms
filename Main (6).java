0606aae805ab6b4e512ad0455b4a813ba9d5a5fe
PantelisRoum <104692010+PantelisRoum@users.noreply.github.com>
2023-03-03 22:05:53 +0200

    Delete Main (6).java

--- Main (6).java
+++
@@ -1,8 +0,0 @@
-//this is the main programm 
-public class Main{
-    public static void main(String[] args){
-        Date date_1=new Date(7,2,2023);
-        date_1.display(7,2,2023);
-        
-    }
-}
